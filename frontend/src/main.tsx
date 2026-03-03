fa2ba9a24ed185367da28514fc6da834492f3312
Rakshit <cs1240020@iitd.ac.in>
2026-03-03 17:42:30 +0530

    removing /tools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <BrowserRouter basename="/tools">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </ClerkProvider>
